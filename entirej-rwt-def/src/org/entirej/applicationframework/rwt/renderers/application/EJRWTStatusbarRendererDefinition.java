63aac0883ea156ccff2e20d749d9f90115bc42da
Anuradha Gunasekara <theanuradha@gmail.com>
2014-01-07 21:05:04 +0530

    CFRCPRAP-111: new StatusBar imll

--- entirej-rwt-def/src/org/entirej/applicationframework/rwt/renderers/application/EJRWTStatusbarRendererDefinition.java
+++ entirej-rwt-def/src/org/entirej/applicationframework/rwt/renderers/application/EJRWTStatusbarRendererDefinition.java
@@ -18,19 +18,82 @@
  ******************************************************************************/
 package org.entirej.applicationframework.rwt.renderers.application;
 
+import org.entirej.framework.core.properties.definitions.EJPropertyDefinitionType;
 import org.entirej.framework.core.properties.definitions.interfaces.EJFrameworkExtensionProperties;
 import org.entirej.framework.core.properties.definitions.interfaces.EJPropertyDefinition;
 import org.entirej.framework.core.properties.definitions.interfaces.EJPropertyDefinitionGroup;
 import org.entirej.framework.core.properties.definitions.interfaces.EJPropertyDefinitionListener;
+import org.entirej.framework.dev.properties.EJDevPropertyDefinition;
+import org.entirej.framework.dev.properties.EJDevPropertyDefinitionGroup;
+import org.entirej.framework.dev.properties.EJDevPropertyDefinitionList;
 import org.entirej.framework.dev.renderer.definition.interfaces.EJDevAppComponentRendererDefinition;
 
 public class EJRWTStatusbarRendererDefinition implements EJDevAppComponentRendererDefinition
 {
 
+    public static final String SECTIONS                  = "SECTIONS";
+    public static final String EXPAND_X                  = "EXPAND_X";
+    public static final String PARAMETER                 = "PARAMETER";
+    public static final String WIDTH                     = "WIDTH";
+    public static final String VISUAL_ATTRIBUTE_PROPERTY = "VISUAL_ATTRIBUTE";
+
+    public static final String PROPERTY_ALIGNMENT        = "ALIGNMENT";
+    public static final String PROPERTY_ALIGNMENT_LEFT   = "LEFT";
+    public static final String PROPERTY_ALIGNMENT_RIGHT  = "RIGHT";
+    public static final String PROPERTY_ALIGNMENT_CENTER = "CENTER";
+
     @Override
     public EJPropertyDefinitionGroup getComponentPropertyDefinitionGroup()
     {
-        return null;
+        EJDevPropertyDefinitionGroup mainGroup = new EJDevPropertyDefinitionGroup("STATUSBARCONFIG");
+        mainGroup.setLabel("StatusBar Configuration");
+
+        EJDevPropertyDefinitionList list = new EJDevPropertyDefinitionList(SECTIONS, "Sections");
+        // TODO: write Section description
+
+        EJDevPropertyDefinition paramater = new EJDevPropertyDefinition(PARAMETER, EJPropertyDefinitionType.STRING);// TODO:
+                                                                                                                    // add
+                                                                                                                    // parameter
+                                                                                                                    // selection
+                                                                                                                    // support
+        paramater.setLabel("Paramater");
+        paramater.setDescription("Application Paramater that this section represent.");// TODO:
+                                                                                       // fix
+                                                                                       // this
+                                                                                       // description
+
+        EJDevPropertyDefinition visualAttribute = new EJDevPropertyDefinition(VISUAL_ATTRIBUTE_PROPERTY, EJPropertyDefinitionType.VISUAL_ATTRIBUTE);
+        visualAttribute.setLabel("Visual Attribute");
+        visualAttribute.setDescription("The visual attribute that should be applied to this item");
+        visualAttribute.setMandatory(false);
+
+        EJDevPropertyDefinition displayedWidth = new EJDevPropertyDefinition(WIDTH, EJPropertyDefinitionType.INTEGER);
+        displayedWidth.setLabel("Displayed Width");
+        displayedWidth
+                .setDescription("Indicates the width of this section. If no value or zero has been entered, the width of the item will be relevent to its contents");
+        displayedWidth.setNotifyWhenChanged(true);
+
+        EJDevPropertyDefinition expandHorizontally = new EJDevPropertyDefinition(EXPAND_X, EJPropertyDefinitionType.BOOLEAN);
+        expandHorizontally.setLabel("Expand Horizontally");
+        expandHorizontally.setDescription("Indicates if this section should expand horizontally when the canvas is stretched");
+        expandHorizontally.setDefaultValue("true");
+
+        EJDevPropertyDefinition textAlignment = new EJDevPropertyDefinition(PROPERTY_ALIGNMENT, EJPropertyDefinitionType.STRING);
+        textAlignment.setLabel("Alignment");
+        textAlignment.setDescription("The alignment of the text within the section");
+        textAlignment.addValidValue(PROPERTY_ALIGNMENT_LEFT, "Left");
+        textAlignment.addValidValue(PROPERTY_ALIGNMENT_RIGHT, "Right");
+        textAlignment.addValidValue(PROPERTY_ALIGNMENT_CENTER, "Center");
+        textAlignment.setDefaultValue(PROPERTY_ALIGNMENT_LEFT);
+
+        list.addPropertyDefinition(paramater);
+        list.addPropertyDefinition(displayedWidth);
+        list.addPropertyDefinition(expandHorizontally);
+        list.addPropertyDefinition(textAlignment);
+        list.addPropertyDefinition(visualAttribute);
+
+        mainGroup.addPropertyDefinitionList(list);
+        return mainGroup;
     }
 
     @Override
--- entirej-rwt/src/org/entirej/applicationframework/rwt/application/components/EJRWTStatusbar.java
+++ entirej-rwt/src/org/entirej/applicationframework/rwt/application/components/EJRWTStatusbar.java
@@ -18,7 +18,13 @@
  ******************************************************************************/
 package org.entirej.applicationframework.rwt.application.components;
 
+
+
+import java.util.List;
+
+import org.eclipse.rwt.EJ_RWT;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -26,33 +32,33 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.entirej.applicationframework.rwt.application.EJRWTApplicationManager;
 import org.entirej.applicationframework.rwt.application.interfaces.EJRWTAppComponentRenderer;
-import org.entirej.applicationframework.rwt.application.interfaces.EJRWTApplicationStatusbar;
+import org.entirej.applicationframework.rwt.utils.EJRWTVisualAttributeUtils;
+import org.entirej.framework.core.data.controllers.EJApplicationLevelParameter;
+import org.entirej.framework.core.data.controllers.EJApplicationLevelParameter.ParameterChangedListener;
+import org.entirej.framework.core.properties.EJCoreProperties;
+import org.entirej.framework.core.properties.EJCoreVisualAttributeProperties;
 import org.entirej.framework.core.properties.definitions.interfaces.EJFrameworkExtensionProperties;
+import org.entirej.framework.core.properties.definitions.interfaces.EJFrameworkExtensionPropertyList;
+import org.entirej.framework.core.properties.definitions.interfaces.EJFrameworkExtensionPropertyListEntry;
 
-public class EJRWTStatusbar implements EJRWTApplicationStatusbar, EJRWTAppComponentRenderer
+public class EJRWTStatusbar implements  EJRWTAppComponentRenderer
 {
-    private Label     status1;
-    private Label     status2;
+    
+    public static final String SECTIONS                  = "SECTIONS";
+    public static final String EXPAND_X                  = "EXPAND_X";
+    public static final String PARAMETER                 = "PARAMETER";
+    public static final String WIDTH                     = "WIDTH";
+    public static final String VISUAL_ATTRIBUTE_PROPERTY = "VISUAL_ATTRIBUTE";
+
+    public static final String PROPERTY_ALIGNMENT        = "ALIGNMENT";
+    public static final String PROPERTY_ALIGNMENT_LEFT   = "LEFT";
+    public static final String PROPERTY_ALIGNMENT_RIGHT  = "RIGHT";
+    public static final String PROPERTY_ALIGNMENT_CENTER = "CENTER";
+
+
     private Composite panel;
 
-    @Override
-    public void setStatus1(String text)
-    {
-        if (status1 != null && !status1.isDisposed())
-        {
-            status1.setText(text != null ? text : "");
-        }
-
-    }
-
-    @Override
-    public void setStatus2(String text)
-    {
-        if (status2 != null && !status2.isDisposed())
-        {
-            status2.setText(text != null ? text : "");
-        }
-    }
+    
 
     @Override
     public Control getGuiComponent()
@@ -63,32 +69,137 @@ public class EJRWTStatusbar implements EJRWTApplicationStatusbar, EJRWTAppCompon
     @Override
     public void createContainer(EJRWTApplicationManager manager, Composite parent, EJFrameworkExtensionProperties rendererprop)
     {
-        int style = SWT.BORDER;
-        GridLayout layout = new GridLayout(3, false);
+        int style = SWT.NONE;
+        
 
         panel = new Composite(parent, style);
+        
+        panel.setData(EJ_RWT.CUSTOM_VARIANT, "applayout");
+        
+        
+        final EJFrameworkExtensionPropertyList propertyList = rendererprop.getPropertyList(SECTIONS);
+
+        if (propertyList == null)
+        {
+            return ;
+        }
+        
+       
+        
+        List<EJFrameworkExtensionPropertyListEntry> allListEntries = propertyList.getAllListEntries();
+        GridLayout layout = new GridLayout(allListEntries.size(), false);
         panel.setLayout(layout);
-        status1 = new Label(panel, SWT.NONE);
-        GridData gridData = new GridData(GridData.FILL_BOTH);
-        gridData.grabExcessHorizontalSpace = true;
-        gridData.grabExcessVerticalSpace = true;
-        gridData.verticalAlignment = GridData.VERTICAL_ALIGN_CENTER;
-        status1.setLayoutData(gridData);
+        for (EJFrameworkExtensionPropertyListEntry entry : allListEntries)
+        {
+            final Label section = new Label(panel,getComponentStyle(entry.getProperty(PROPERTY_ALIGNMENT),SWT.NONE));
+            
+            String paramName = entry.getProperty(PARAMETER);
+            if(paramName!=null && paramName.length()>0)
+            {
+                
+                final EJApplicationLevelParameter applicationLevelParameter = manager.getApplicationLevelParameter(paramName);
+                if(applicationLevelParameter!=null)
+                {
+                    Object value = applicationLevelParameter.getValue();
+                    section.setText(value==null?"":value.toString());
+                    applicationLevelParameter.addParameterChangedListener(new ParameterChangedListener()
+                    {
+                        
+                        @Override
+                        public void parameterChanged(String parameterName, Object oldValue, Object newValue)
+                        {
+                            section.setText(newValue==null?"":newValue.toString());
+                            
+                        }
+                    });
+                }
+                
+            }
+            GridData gridData = new GridData(GridData.FILL_BOTH);
+            gridData.grabExcessVerticalSpace = true;
+            section.setLayoutData(gridData);
+            
+            boolean expand = Boolean.valueOf(entry.getProperty(EXPAND_X));
+            if(expand)
+            {
+                gridData.grabExcessHorizontalSpace = true;
+            }
+            
+            String width = entry.getProperty(WIDTH);
+          
+            
+            if(width!=null && width.length()>0)
+            {
+                try
+                {
+                    gridData.widthHint = (Integer.parseInt(width));
+                }
+                catch(Exception ex)
+                {
+                    //ignore
+                }
+                
+            }
+           
+            
+            //set VA 
+            
+            String visualAttribute = entry.getProperty(VISUAL_ATTRIBUTE_PROPERTY);
+            if(visualAttribute!=null && visualAttribute.length()>0)
+            {
+                
+                EJCoreVisualAttributeProperties va = EJCoreProperties.getInstance().getVisualAttributesContainer()
+                        .getVisualAttributeProperties(visualAttribute);
+                if (va != null)
+                {
+                    
+                        Color background = EJRWTVisualAttributeUtils.INSTANCE.getBackground(va);
+                        if(background != null)
+                        {
+                            section.setBackground(background);
+                        }
+                    
 
-        gridData = new GridData(GridData.FILL_BOTH);
+                    
+                        Color foreground = EJRWTVisualAttributeUtils.INSTANCE.getForeground(va);
+                        if(foreground != null)
+                        {
+                            section.setForeground(foreground);
+                        }
+                    
 
-        gridData.grabExcessHorizontalSpace = false;
-        gridData.grabExcessVerticalSpace = false;
-        gridData.widthHint = 5;
-        gridData.verticalIndent = 0;
-        new Label(panel, SWT.SEPARATOR).setLayoutData(gridData);
+                    
+                        section.setFont(EJRWTVisualAttributeUtils.INSTANCE.getFont(va, section.getFont()));
+                    
+                }
+            }
+            //
+            
+        }
+        
+        
+        
 
-        status2 = new Label(panel, SWT.NONE);
-        gridData = new GridData(GridData.FILL_BOTH);
-        gridData.grabExcessHorizontalSpace = false;
-        gridData.grabExcessVerticalSpace = true;
-        gridData.widthHint = 120;
-        gridData.verticalAlignment = GridData.VERTICAL_ALIGN_CENTER;
-        status2.setLayoutData(gridData);
+    }
+    
+    
+    protected int getComponentStyle(String alignmentProperty, int style)
+    {
+        if (alignmentProperty != null && alignmentProperty.trim().length() > 0)
+        {
+            if (alignmentProperty.equals(PROPERTY_ALIGNMENT_LEFT))
+            {
+                style = style | SWT.LEFT;
+            }
+            else if (alignmentProperty.equals(PROPERTY_ALIGNMENT_RIGHT))
+            {
+                style = style | SWT.RIGHT;
+            }
+            else if (alignmentProperty.equals(PROPERTY_ALIGNMENT_CENTER))
+            {
+                style = style | SWT.CENTER;
+            }
+        }
+        return style;
     }
 }
