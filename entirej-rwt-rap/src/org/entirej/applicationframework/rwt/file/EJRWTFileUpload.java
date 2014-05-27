a5e5551dbb4ee4885766fd0bd9e42db0758b43d1
Anuradha Gunasekara <theanuradha@gmail.com>
2014-05-27 12:16:34 +0530

    public static String[] propmptMultipleFileUpload(String title)

---
+++ entirej-rwt-rap/src/org/entirej/applicationframework/rwt/file/EJRWTFileUpload.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright 2014 Mojave Innovations GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributors:
+ *     Mojave Innovations GmbH - initial API and implementation
+ ******************************************************************************/
+package org.entirej.applicationframework.rwt.file;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+
+public class EJRWTFileUpload
+{
+    private EJRWTFileUpload()
+    {
+    }
+
+    public static String propmptFileUpload(String title)
+    {
+
+        FileDialog fileDialog = new FileDialog(Display.getDefault().getActiveShell(), SWT.SHELL_TRIM | SWT.APPLICATION_MODAL | SWT.SINGLE);
+        fileDialog.setText(title);
+        fileDialog.open();
+        return fileDialog.getFileName();
+    }
+
+    public static String[] propmptMultipleFileUpload(String title)
+    {
+
+        FileDialog fileDialog = new FileDialog(Display.getDefault().getActiveShell(), SWT.SHELL_TRIM | SWT.APPLICATION_MODAL | SWT.MULTI);
+        fileDialog.setText(title);
+        fileDialog.open();
+        return fileDialog.getFileNames();
+    }
+}
