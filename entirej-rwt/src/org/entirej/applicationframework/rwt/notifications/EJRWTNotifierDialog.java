ec0f627f6feeee17e0e786f2c65cdbce56f11297
Anuradha Gunasekara <theanuradha@gmail.com>
2014-07-06 15:12:35 +0530

    Sync Events when about to show notifications

--- entirej-rwt/src/org/entirej/applicationframework/rwt/notifications/EJRWTNotifierDialog.java
+++ entirej-rwt/src/org/entirej/applicationframework/rwt/notifications/EJRWTNotifierDialog.java
@@ -73,7 +73,6 @@ public class EJRWTNotifierDialog
     // contains list of all active popup shells
     private static List<Shell> _activeShells = new ArrayList<Shell>();
 
-
     /**
      * Creates and shows a notification dialog with a specific title, message
      * and a
@@ -82,122 +81,132 @@ public class EJRWTNotifierDialog
      * @param message
      * @param type
      */
-    public static void notify(String title, String message, Image image, int width, int height, boolean autoHide)
+    public static void notify(final String title, final String message, final Image image, final int width, final int height, final boolean autoHide)
     {
-       final Shell _shell = new Shell(Display.getDefault().getActiveShell(), SWT.NO_FOCUS | SWT.NO_TRIM);
-        _shell.setLayout(new FillLayout());
-        _shell.setForeground(_fgColor);
-        _shell.setBackgroundMode(SWT.INHERIT_DEFAULT);
-        _shell.addListener(SWT.Dispose, new Listener()
+        Display.getCurrent().asyncExec(new Runnable()
         {
+
             @Override
-            public void handleEvent(Event event)
+            public void run()
             {
-                if (_activeShells != null && _shell!=null )
-                    _activeShells.remove(_shell);
-            }
-        });
-
-        final Composite inner = new Composite(_shell, SWT.BORDER);
-        inner.setData(EJ_RWT.CUSTOM_VARIANT, "notifierDialog");
-        GridLayout gl = new GridLayout(3, false);
-        gl.marginLeft = 5;
-        gl.marginTop = 0;
-        gl.marginRight = 5;
-        gl.marginBottom = 5;
-
-        inner.setLayout(gl);
-
-        GC gc = new GC(_shell);
-
-        String lines[] = message.split("\n");
-        Point longest = null;
-
-        for (String line : lines)
-        {
-            Point extent = gc.stringExtent(line);
-            if (longest == null)
-            {
-                longest = extent;
-                continue;
-            }
-
-            if (extent.x > longest.x)
-            {
-                longest = extent;
-            }
-        }
-        gc.dispose();
-
-        CLabel imgLabel = new CLabel(inner, SWT.NONE);
-        imgLabel.setLayoutData(new GridData(GridData.FILL | GridData.FILL));
-        imgLabel.setImage(image);
-
-        CLabel titleLabel = new CLabel(inner, SWT.NONE);
-        titleLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_CENTER));
-        titleLabel.setText(title);
-        titleLabel.setForeground(_titleFgColor);
-        Font f = titleLabel.getFont();
-        FontData fd = f.getFontData()[0];
-        titleLabel.setFont(new Font(Display.getDefault(), new FontData(fd.getName(), fd.getHeight(), SWT.BOLD)));
-
-        final CLabel closeLabel = new CLabel(inner, SWT.NONE);
-        closeLabel.setLayoutData(new GridData(GridData.FILL | GridData.FILL));
-        closeLabel.setImage(EJRWTImageRetriever.get(EJRWTImageRetriever.IMG_CLOSE));
-        closeLabel.addMouseListener(new MouseAdapter()
-        {
-            @Override
-            public void mouseUp(MouseEvent e)
-            {
-                fadeOut(closeLabel.getShell());
-            }
-        });
-
-        Label text = new Label(inner, SWT.WRAP);
-
-        text.setFont(new Font(Display.getDefault(), new FontData(fd.getName(), fd.getHeight() - 2, SWT.NONE)));
-        GridData gd = new GridData(GridData.FILL_BOTH);
-        gd.horizontalSpan = 3;
-        text.setLayoutData(gd);
-        text.setForeground(_fgColor);
-        text.setText(message);
-
-        _shell.setSize(width, height);
-
-        if (Display.getDefault().getActiveShell() == null || Display.getDefault().getActiveShell().getMonitor() == null)
-        {
-            return;
-        }
-
-        Rectangle clientArea = Display.getDefault().getActiveShell().getMonitor().getClientArea();
-
-        int startX = clientArea.x + clientArea.width - (width + 2);
-        int startY = clientArea.y + clientArea.height - (height + 2);
-
-        // move other shells up
-        if (!_activeShells.isEmpty())
-        {
-            List<Shell> modifiable = new ArrayList<Shell>(_activeShells);
-            Collections.reverse(modifiable);
-            for (Shell shell : modifiable)
-            {
-                Point curLoc = shell.getLocation();
-                shell.setLocation(curLoc.x, curLoc.y - 100);
-                if (curLoc.y - 100 < 0)
+                final Shell _shell = new Shell(Display.getDefault().getActiveShell(), SWT.NO_FOCUS | SWT.NO_TRIM);
+                _shell.setLayout(new FillLayout());
+                _shell.setForeground(_fgColor);
+                _shell.setBackgroundMode(SWT.INHERIT_DEFAULT);
+                _shell.addListener(SWT.Dispose, new Listener()
                 {
-                    _activeShells.remove(shell);
-                    shell.dispose();
+                    @Override
+                    public void handleEvent(Event event)
+                    {
+                        if (_activeShells != null && _shell != null)
+                            _activeShells.remove(_shell);
+                    }
+                });
+
+                final Composite inner = new Composite(_shell, SWT.BORDER);
+                inner.setData(EJ_RWT.CUSTOM_VARIANT, "notifierDialog");
+                GridLayout gl = new GridLayout(3, false);
+                gl.marginLeft = 5;
+                gl.marginTop = 0;
+                gl.marginRight = 5;
+                gl.marginBottom = 5;
+
+                inner.setLayout(gl);
+
+                GC gc = new GC(_shell);
+
+                String lines[] = message.split("\n");
+                Point longest = null;
+
+                for (String line : lines)
+                {
+                    Point extent = gc.stringExtent(line);
+                    if (longest == null)
+                    {
+                        longest = extent;
+                        continue;
+                    }
+
+                    if (extent.x > longest.x)
+                    {
+                        longest = extent;
+                    }
                 }
+                gc.dispose();
+
+                CLabel imgLabel = new CLabel(inner, SWT.NONE);
+                imgLabel.setLayoutData(new GridData(GridData.FILL | GridData.FILL));
+                imgLabel.setImage(image);
+
+                CLabel titleLabel = new CLabel(inner, SWT.NONE);
+                titleLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_CENTER));
+                titleLabel.setText(title);
+                titleLabel.setForeground(_titleFgColor);
+                Font f = titleLabel.getFont();
+                FontData fd = f.getFontData()[0];
+                titleLabel.setFont(new Font(Display.getDefault(), new FontData(fd.getName(), fd.getHeight(), SWT.BOLD)));
+
+                final CLabel closeLabel = new CLabel(inner, SWT.NONE);
+                closeLabel.setLayoutData(new GridData(GridData.FILL | GridData.FILL));
+                closeLabel.setImage(EJRWTImageRetriever.get(EJRWTImageRetriever.IMG_CLOSE));
+                closeLabel.addMouseListener(new MouseAdapter()
+                {
+                    @Override
+                    public void mouseUp(MouseEvent e)
+                    {
+                        fadeOut(closeLabel.getShell());
+                    }
+                });
+
+                Label text = new Label(inner, SWT.WRAP);
+
+                text.setFont(new Font(Display.getDefault(), new FontData(fd.getName(), fd.getHeight() - 2, SWT.NONE)));
+                GridData gd = new GridData(GridData.FILL_BOTH);
+                gd.horizontalSpan = 3;
+                text.setLayoutData(gd);
+                text.setForeground(_fgColor);
+                text.setText(message);
+
+                _shell.setSize(width, height);
+
+                if (Display.getDefault().getActiveShell() == null || Display.getDefault().getActiveShell().getMonitor() == null)
+                {
+                    return;
+                }
+
+                Rectangle clientArea = Display.getDefault().getActiveShell().getMonitor().getClientArea();
+
+                int startX = clientArea.x + clientArea.width - (width + 2);
+                int startY = clientArea.y + clientArea.height - (height + 2);
+
+                // move other shells up
+                if (!_activeShells.isEmpty())
+                {
+                    List<Shell> modifiable = new ArrayList<Shell>(_activeShells);
+                    Collections.reverse(modifiable);
+                    for (Shell shell : modifiable)
+                    {
+                        Point curLoc = shell.getLocation();
+                        shell.setLocation(curLoc.x, curLoc.y - 100);
+                        if (curLoc.y - 100 < 0)
+                        {
+                            _activeShells.remove(shell);
+                            shell.dispose();
+                        }
+                    }
+                }
+
+                _shell.setLocation(startX, startY);
+                _shell.setAlpha(0);
+                _shell.setVisible(true);
+
+                _activeShells.add(_shell);
+
+                fadeIn(_shell, autoHide);
+
             }
-        }
+        });
 
-        _shell.setLocation(startX, startY);
-        _shell.setAlpha(0);
-        _shell.setVisible(true);
-
-        _activeShells.add(_shell);
-
-        fadeIn(_shell, autoHide);
     }
 
     private static void fadeIn(final Shell _shell, final boolean autoHide)
