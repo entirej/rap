40131ec617f741f5a105759dc1319692012a97d8
Anuradha Gunasekara <theanuradha@gmail.com>
2015-06-05 12:47:20 +0530

    Revert "fixes related to update rap 3.0"
    This reverts commit ce790f2d2b479ebdd43025312f54090f989985dd.

--- entirej-rwt-rap/src/org/entirej/applicationframework/rwt/application/launcher/EJRWTApplicationLauncher.java
+++ entirej-rwt-rap/src/org/entirej/applicationframework/rwt/application/launcher/EJRWTApplicationLauncher.java
@@ -160,7 +160,6 @@ public abstract class EJRWTApplicationLauncher implements ApplicationConfigurati
     public void createEntryPoint(final Application configuration)
     {
 
-        configuration.setOperationMode(OperationMode.SWT_COMPATIBILITY);
         Map<String, String> properties = new HashMap<String, String>();
         if (this.getClass().getClassLoader().getResource("application.ejprop") != null)
         {
@@ -376,15 +375,6 @@ public abstract class EJRWTApplicationLauncher implements ApplicationConfigurati
     {
         shell.open();
         
-            while (!shell.isDisposed())
-            {
-                if (!display.readAndDispatch())
-                {
-                    display.sleep();
-                }
-            }
-        
-
         return 0;
     }
 
