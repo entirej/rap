371c16fe7a70871ec81003442f04c173bf3d897f
Anuradha Gunasekara <theanuradha@gmail.com>
2016-12-12 18:10:43 +0530

    fix for dispose

--- entirej-rwt/src/org/entirej/applicationframework/rwt/layout/EJRWTScrolledComposite.java
+++ entirej-rwt/src/org/entirej/applicationframework/rwt/layout/EJRWTScrolledComposite.java
@@ -1,8 +1,11 @@
 package org.entirej.applicationframework.rwt.layout;
 
 import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 
 public class EJRWTScrolledComposite extends ScrolledComposite
 {
@@ -11,6 +14,24 @@ public class EJRWTScrolledComposite extends ScrolledComposite
     {
         super(parent, style);
 
+    }
+    
+    @Override
+    public void setContent(final Control content)
+    {
+       if(content!=null){
+           
+           content.addDisposeListener(new DisposeListener()
+        {
+            
+            @Override
+            public void widgetDisposed(DisposeEvent event)
+            {
+                EJRWTScrolledComposite.super.setContent(null);
+            }
+        });
+       }
+        super.setContent(content);
     }
 
     @Override
