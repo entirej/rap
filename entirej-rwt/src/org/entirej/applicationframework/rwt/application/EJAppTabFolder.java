926b8f724a897e5189031e19b3cae370e7c7b986
Anuradha Gunasekara <theanuradha@gmail.com>
2026-08-20 10:56:41 +0200

    Fix application tab enable handling

--- entirej-rwt/src/org/entirej/applicationframework/rwt/application/EJAppTabFolder.java
+++ entirej-rwt/src/org/entirej/applicationframework/rwt/application/EJAppTabFolder.java
@@ -7,7 +7,6 @@ import org.eclipse.rap.rwt.RWT;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
-import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 
@@ -178,7 +177,11 @@ class EJAppTabFolder
         CTabItem cTabItem = tabPages.get(tabPageName);
         if (cTabItem != null)
         {
-            ((ScrolledComposite)cTabItem.getControl()).getContent().setEnabled(enable);
+            Control control = cTabItem.getControl();
+            if (control != null && !control.isDisposed())
+            {
+                control.setEnabled(enable);
+            }
         }
         
     }
